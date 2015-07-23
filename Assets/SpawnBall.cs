185803f7c422c37a49acc6846e403effae8fad3c
zabaglione <z.zabaglione@gmail.com>
2015-07-24 00:31:50 +0900

    Init

---
+++ Assets/SpawnBall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBall : MonoBehaviour {
+
+	public GameObject ball;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Spawn() {
+		Instantiate(ball);
+	}
+}
